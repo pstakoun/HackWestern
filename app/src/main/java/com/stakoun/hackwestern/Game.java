d4ef6559f441cc05074b5ebdbb68f42b4692f633
Peter Stakoun <peter@stakoun.com>
2015-11-28 03:00:58 -0500

    Update

--- app/src/main/java/com/stakoun/hackwestern/Game.java
+++ app/src/main/java/com/stakoun/hackwestern/Game.java
@@ -1,9 +1,9 @@
 package com.stakoun.hackwestern;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -11,34 +11,41 @@ import java.net.Socket;
 public class Game
 {
     private String id;
-    private Socket socket;
-    PrintWriter out;
-    BufferedReader in;
 
     public Game(String id)
     {
         this.id = id;
-        try {
-            socket = new Socket("hackwestern-kshen3778.c9users.io", 8080);
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            listen();
-        } catch (Exception e) {
-            Log.e("Socket", "error");
-        }
-    }
-
-    private void listen() throws IOException
-    {
-        String line;
-        while ((line = in.readLine()) != "exit") {
-            out.println("test");
-        }
+        (new SocketTask()).execute();
     }
 
     public static Game fromId(String id)
     {
         return new Game(id);
+    }
+
+}
+
+class SocketTask extends AsyncTask<Void, Void, Void>
+{
+    private Socket socket;
+    PrintWriter out;
+    BufferedReader in;
+
+    @Override
+    public Void doInBackground(Void... voids) {
+        try {
+            socket = new Socket("hackwestern-kshen3778.c9users.io", 8080);
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                Log.d("IN", line);
+                out.println("test");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
