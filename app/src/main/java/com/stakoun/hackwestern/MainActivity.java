00fc956e951a9bc26b7306b20eb95c481528802a
Peter Stakoun <peter@stakoun.com>
2015-11-28 21:11:34 -0500

    Update

--- app/src/main/java/com/stakoun/hackwestern/MainActivity.java
+++ app/src/main/java/com/stakoun/hackwestern/MainActivity.java
@@ -1,9 +1,15 @@
 package com.stakoun.hackwestern;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -16,9 +22,38 @@ public class MainActivity extends AppCompatActivity
 
     public void playGame(View v)
     {
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-        finish();
+        GetTypeTask gtt = new GetTypeTask();
+        gtt.execute();
+        String s = gtt.getType();
+
+        if (s == "assassin") {
+            Intent intent = new Intent(this, AssassinActivity.class);
+            startActivity(intent);
+            finish();
+        } else if (s == "victim") {
+            Intent intent = new Intent(this, VictimActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+}
+
+class GetTypeTask extends AsyncTask<Void, Void, Void> {
+    String type;
+
+    @Override
+    public Void doInBackground(Void... voids) {
+        try {
+            type = (new BufferedReader(new InputStreamReader((new Socket("52.24.241.238", 8080)).getInputStream()))).readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public String getType() {
+        return type;
     }
 
 }
--- app/src/main/java/com/stakoun/hackwestern/VictimActivity.java
+++ app/src/main/java/com/stakoun/hackwestern/VictimActivity.java
@@ -1,16 +1,12 @@
 package com.stakoun.hackwestern;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class GameActivity extends AppCompatActivity
-{
-    public Game game;
-
+public class VictimActivity extends GameActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        game = new Game(this);
+        setContentView(R.layout.activity_victim);
     }
 
 }
